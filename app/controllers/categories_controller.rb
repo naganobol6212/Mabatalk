db2f8168d031fb980533df76429f972950812cc7
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2025-12-27 15:13:32 +0900

    質問カテゴリ一覧ページを実装

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,20 @@
 class CategoriesController < ApplicationController
   def index
+    if current_user
+      @categories = current_user.categories
+    else
+      @categories = default_categories
+    end
+  end
+
+  private
+
+  def default_categories
+    [
+      { icon: "local_cafe", title: "飲みもの", ruby: "のみもの", color: "text-amber-700" },
+      { icon: "accessibility_new", title: "体調", ruby: "からだのちょうし", color: "text-sky-600" },
+      { icon: "sentiment_satisfied_alt", title: "気持ち", ruby: "いまのきもち", color: "text-rose-500" },
+      { icon: "volunteer_activism", title: "お願い", ruby: "やってほしいこと", color: "text-emerald-600" }
+    ]
   end
 end
