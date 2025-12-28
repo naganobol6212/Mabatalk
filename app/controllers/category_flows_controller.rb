12bddd80c9b36c3294fad553750e0ec6b9036862
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2025-12-28 13:29:41 +0900

    フロー画面の設計整理しコードを修正

--- app/controllers/category_flows_controller.rb
+++ app/controllers/category_flows_controller.rb
@@ -1,22 +1,30 @@
 class CategoryFlowsController < ApplicationController
   def show
-    @category = { title: "体調" } # 仮データ
-    @items = flow_items || []
+    cid = params[:category_id].to_i
+    data = category_flow_data[cid] || { title: "不明", items: [] }
+
+    @category = { title: data[:title] }
+    @items    = data[:items]
   end
 
   private
 
-  def flow_items
+  def category_flow_data
     {
-      1 => [
-        { name: "暑い", kana: "あつい", icon: "Web_sunny"},
-        { name: "寒い", kana: "さむい", icon: "ac_unit" },
-        { name: "痛い", kana: "いたい", icon: "sick" }
-      ],
-      2 => [
-        { name: "うれしい", kana: "うれしい", icon: "sentiment_satisfied" },
-        { name: "かなしい", kana: "かなしい", icon: "sentiment_dissatisfied" }
-      ]
-    }[params[:id].to_i]
+      1 => {
+        title: "体調",
+        items: [
+          { name: "暑い", kana: "あつい", icon: "wb_sunny" },
+          { name: "寒い", kana: "さむい", icon: "ac_unit" }
+        ]
+      },
+      2 => {
+        title: "気持ち",
+        items: [
+          { name: "うれしい", kana: "うれしい", icon: "sentiment_satisfied" },
+          { name: "かなしい", kana: "かなしい", icon: "sentiment_dissatisfied" }
+        ]
+      }
+    }
   end
 end
