b3d72ad83775e4738c2992653afc419a51fe516b
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-01-05 14:34:49 +0900

    フロー項目選択後にメッセージ確認画面へ遷移する

--- app/controllers/category_flows_controller.rb
+++ app/controllers/category_flows_controller.rb
@@ -7,8 +7,5 @@ class CategoryFlowsController < ApplicationController
     @items = @category.flow_items
       .for_user(current_user)
       .order(:position)
-
-    selected_key = params[:item].presence
-    @selected_item = @items.find { |item| item.key == selected_key }
   end
 end
