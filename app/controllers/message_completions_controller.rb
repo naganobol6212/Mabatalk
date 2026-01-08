b3a86fcfd2fc2bc82da122dc1af857542ad17808
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-01-09 06:30:15 +0900

    未ログイン状態でメッセージ確認画面「はい」選択後、完了画面へ遷移

--- app/controllers/message_completions_controller.rb
+++ app/controllers/message_completions_controller.rb
@@ -1,5 +1,10 @@
 class MessageCompletionsController < ApplicationController
   def show
-    @message_log = current_user.message_logs.order(created_at: :desc).first
+    if user_signed_in?
+      @flow_item =
+        current_user.message_logs.order(created_at: :desc).first&.flow_item
+    else
+      @flow_item = FlowItem.find_by(id: session[:last_flow_item_id])
+    end
   end
 end
--- app/controllers/message_logs_controller.rb
+++ app/controllers/message_logs_controller.rb
@@ -1,12 +1,16 @@
 class MessageLogsController < ApplicationController
   def create
-    return redirect_to message_completion_path unless user_signed_in?
-    flow_item = FlowItem.find(params[:flow_item_id])
-    message_log = MessageLog.new(
-      flow_item: flow_item,
-      user: current_user
-    )
-    message_log.save
-    redirect_to message_completion_path, status: :see_other
+  flow_item = FlowItem.find(params[:flow_item_id])
+
+    if user_signed_in?
+      MessageLog.create!(
+        user: current_user,
+        flow_item: flow_item
+      )
+    else
+      session[:last_flow_item_id] = flow_item.id
+    end
+
+    redirect_to message_completion_path
   end
 end
