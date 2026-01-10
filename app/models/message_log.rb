19c4e697becc661ce735a10ac49b71afc16ef2cc
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-01-10 17:13:41 +0900

    メッセージログ一覧ページのcontrollerと取得scopeを実装

--- app/models/message_log.rb
+++ app/models/message_log.rb
@@ -1,4 +1,8 @@
 class MessageLog < ApplicationRecord
   belongs_to :user
   belongs_to :flow_item
+
+  scope :for_viewer, ->(viewer){
+    where(user_id: viewer.id).order(created_at: :desc)
+  }
 end
