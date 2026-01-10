ff157632d25b714f0721ed171dff193930f6532a
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-01-10 09:36:40 +0900

    Categoryモデルにkeyのバリデーション追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ApplicationRecord
   has_many :flow_items, dependent: :destroy
   belongs_to :user, optional: true
 
+  validates :key, presence: true, uniqueness: true
+
   scope :for_user, ->(user) {
     if user
       where(user_id: [ nil, user.id ])
