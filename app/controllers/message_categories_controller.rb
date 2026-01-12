b5de8beb156e73fca18b2072cd36997704760413
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-01-12 10:32:08 +0900

    ログインユーザーに紐づくメッセージカテゴリ作成処理を実装

---
+++ app/controllers/message_categories_controller.rb
@@ -0,0 +1,25 @@
+class MessageCategoriesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @message_category = MessageCategory.new
+  end
+
+  def create
+    @message_category =
+      current_user.message_categories.build(message_category_params)
+
+    if @message_category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def message_category_params
+    params.require(:message_category)
+          .permit(:name, :kana, :icon)
+  end
+end
