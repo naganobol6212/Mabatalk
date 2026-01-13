8d711ebec2a7b3ea2a86422bb9257aa565a6e8db
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-01-13 09:39:00 +0900

    メッセージカテゴリ追加画面のUI実装

---
+++ app/helpers/message_categories_helper.rb
@@ -0,0 +1,10 @@
+module MessageCategoriesHelper
+  def message_category_icons
+    {
+      "local_cafe" => "飲みもの",
+      "accessibility_new" => "体調",
+      "sentiment_satisfied_alt" => "気持ち",
+      "volunteer_activism" => "お願い"
+    }
+  end
+end
