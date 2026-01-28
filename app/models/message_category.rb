6d63d6d6515a6217e69202f052dcb2278c0d2e71
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-01-29 08:02:46 +0900

    lintエラー修正

--- app/models/message_category.rb
+++ app/models/message_category.rb
@@ -6,7 +6,7 @@ class MessageCategory < ApplicationRecord
 
   scope :for_user, ->(user) {
     if user
-      where(user_id: [nil, user.id])
+      where(user_id: [ nil, user.id ])
     else
       where(user_id: nil)
     end
