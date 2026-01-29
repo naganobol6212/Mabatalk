39c5e61d3fb1f61e8553b388d9b452a9dda8c0c1
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-01-29 13:56:27 +0900

    MessageCategoryのseedを実カラム構成に合わせて修正

--- db/seeds/message_categories.rb
+++ db/seeds/message_categories.rb
@@ -1,31 +1,31 @@
 MessageCategory.find_or_create_by!(key: "body") do |c|
-  c.title    = "体調"
-  c.ruby     = "からだのちょうし"
+  c.name    = "体調"
+  c.kana     = "からだのちょうし"
   c.icon     = "accessibility_new"
-  c.color    = "text-orange-600"
+  c.icon_color    = "text-orange-600"
   c.position = 1
 end
 
 MessageCategory.find_or_create_by!(key: "drink") do |c|
-  c.title    = "飲みもの"
-  c.ruby     = "のみもの"
+  c.name    = "飲みもの"
+  c.kana     = "のみもの"
   c.icon     = "local_drink"
-  c.color    = "text-sky-600"
+  c.icon_color    = "text-sky-600"
   c.position = 2
 end
 
 MessageCategory.find_or_create_by!(key: "request") do |c|
-  c.title    = "お願い"
-  c.ruby     = "やってほしいこと"
+  c.name    = "お願い"
+  c.kana     = "やってほしいこと"
   c.icon     = "volunteer_activism"
-  c.color    = "text-emerald-600"
+  c.icon_color    = "text-emerald-600"
   c.position = 3
 end
 
 MessageCategory.find_or_create_by!(key: "feeling") do |c|
-  c.title    = "気持ち"
-  c.ruby     = "いまのきもち"
+  c.name    = "気持ち"
+  c.kana     = "いまのきもち"
   c.icon     = "sentiment_satisfied_alt"
-  c.color    = "text-rose-500"
+  c.icon_color    = "text-rose-500"
   c.position = 4
 end
