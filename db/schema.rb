ee0d58eb2ac84ffdad4d0bea942fa37af532eb8a
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-01-30 11:12:29 +0900

    fix: MessageLog作成時の500エラーを修正し、不要カラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_28_085018) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_30_015846) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,8 +62,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_28_085018) do
     t.bigint "flow_item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "message_category_name", null: false
-    t.string "flow_item_name", null: false
     t.index ["flow_item_id"], name: "index_message_logs_on_flow_item_id"
     t.index ["user_id"], name: "index_message_logs_on_user_id"
   end
