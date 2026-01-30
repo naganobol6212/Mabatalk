b41b88e0fb36232015149f847abd8076202f75b9
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-01-30 15:44:02 +0900

    fix: FlowItemを正しいMessageCategoryに再割り当てし、未紐付け状態を解消

--- db/seeds/flow_items_request.rb
+++ db/seeds/flow_items_request.rb
@@ -36,7 +36,7 @@ items = [
 ]
 
 items.each do |attrs|
-  item = FlowItem.find_or_initialize_by(key: attrs[:key])
+  item = FlowItem.find_or_initialize_by(key: attrs[:key], user_id: nil)
   item.assign_attributes(attrs.merge(
     user_id: nil,
     message_category: request
