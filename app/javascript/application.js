1aca50c4f4335b0cadf9ee50fd37d24509d8b073
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-02-12 16:45:17 +0900

    GA4(Google Analytics)を本番環境に導入(Turbo対応)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+document.addEventListener("turbo:load", () => {
+  if (window.gtag) {
+    window.gtag("event", "page_view", {
+      page_path: window.location.pathname,
+      page_location: window.location.href,
+      page_title: document.title
+    });
+  }
+});
