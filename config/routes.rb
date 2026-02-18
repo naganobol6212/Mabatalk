92e0a0bb0bd3e3b6b1c65151f8080ec8e2ea8ca9
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-02-18 18:13:43 +0900

    chore: development環境にletter_opener_webを追加

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
