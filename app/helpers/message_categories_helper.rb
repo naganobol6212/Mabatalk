39f11fe3c180d90a249d3309b66ecc4ff3273e4b
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-02-20 15:24:12 +0900

    refactor: icon定義をlibに集約しhelperを参照化

--- app/helpers/message_categories_helper.rb
+++ app/helpers/message_categories_helper.rb
@@ -1,19 +1,9 @@
 module MessageCategoriesHelper
   def message_category_icons
-    {
-      "local_cafe" => "飲みもの",
-      "accessibility_new" => "体調",
-      "sentiment_satisfied_alt" => "気持ち",
-      "volunteer_activism" => "お願い"
-    }
+    IconDefinitions::CATEGORY_ICONS.transform_values { |v| v[:label] }
   end
 
   def icon_color(icon)
-    {
-      "local_cafe" => "text-orange-500",
-      "accessibility_new" => "text-sky-500",
-      "sentiment_satisfied_alt" => "text-rose-400",
-      "volunteer_activism" => "text-emerald-500"
-    }[icon] || "text-gray-400"
+    IconDefinitions::CATEGORY_ICONS.dig(icon, :color) || "text-gray-400"
   end
 end
