6e532dda2a55e1729ef70d41dd0f4aeb07606709
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-02-24 12:42:36 +0900

    feat: 完了画面・ログ一覧の表示をスナップショットフィールドに移行する

--- app/controllers/message_completions_controller.rb
+++ app/controllers/message_completions_controller.rb
@@ -1,13 +1,27 @@
 class MessageCompletionsController < ApplicationController
   def show
-    flow_item_id =
-      if user_signed_in?
-        MessageLog.order(created_at: :desc).find_by(user: current_user)&.flow_item_id
-      else
-        session[:last_flow_item_id]
-      end
+    if user_signed_in?
+      last_log = MessageLog.for_viewer(current_user).first
+      assign_from_log(last_log) if last_log
+    else
+      flow_item = FlowItem.find_by(id: session[:last_flow_item_id])
+      assign_from_flow_item(flow_item) if flow_item
+    end
+  end
 
-    @flow_item = FlowItem.find(flow_item_id)
-    @message_category = @flow_item.message_category
+  private
+
+  def assign_from_log(log)
+    @flow_item_name              = log.flow_item_name
+    @flow_item_icon              = log.flow_item_icon
+    @flow_item_icon_color        = log.flow_item_icon_color
+    @message_category_name       = log.message_category_name
+  end
+
+  def assign_from_flow_item(flow_item)
+    @flow_item_name              = flow_item.name
+    @flow_item_icon              = flow_item.icon
+    @flow_item_icon_color        = flow_item.icon_color
+    @message_category_name       = flow_item.message_category.name
   end
 end
