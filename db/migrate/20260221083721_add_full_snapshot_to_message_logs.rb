6219928f8b6b2fc555db7fe5fbb6289ff481aabf
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-02-24 14:44:52 +0900

    fix: AddFullSnapshotToMessageLogsをidempotentに修正する

--- db/migrate/20260221083721_add_full_snapshot_to_message_logs.rb
+++ db/migrate/20260221083721_add_full_snapshot_to_message_logs.rb
@@ -1,9 +1,15 @@
 class AddFullSnapshotToMessageLogs < ActiveRecord::Migration[7.2]
-  def change
-    add_column :message_logs, :message_category_name,       :string
-    add_column :message_logs, :flow_item_name,              :string
-    add_column :message_logs, :flow_item_icon,              :string
-    add_column :message_logs, :flow_item_icon_color,        :string
-    add_column :message_logs, :message_category_icon_color, :string
+  def up
+    add_column :message_logs, :message_category_name,       :string unless column_exists?(:message_logs, :message_category_name)
+    add_column :message_logs, :flow_item_name,              :string unless column_exists?(:message_logs, :flow_item_name)
+    add_column :message_logs, :flow_item_icon,              :string unless column_exists?(:message_logs, :flow_item_icon)
+    add_column :message_logs, :flow_item_icon_color,        :string unless column_exists?(:message_logs, :flow_item_icon_color)
+    add_column :message_logs, :message_category_icon_color, :string unless column_exists?(:message_logs, :message_category_icon_color)
+  end
+
+  def down
+    remove_column :message_logs, :flow_item_icon              if column_exists?(:message_logs, :flow_item_icon)
+    remove_column :message_logs, :flow_item_icon_color        if column_exists?(:message_logs, :flow_item_icon_color)
+    remove_column :message_logs, :message_category_icon_color if column_exists?(:message_logs, :message_category_icon_color)
   end
 end
