ebb308deb788c636cf5bc9c2d766c0504ad9a673
長野理弥 <naganoma@naganoosamuwatarunoMacBook-Air.local>
2026-02-28 18:07:07 +0900

    feat: 詳細フローの回答を MessageLog に保存・表示する

--- app/controllers/message_completions_controller.rb
+++ app/controllers/message_completions_controller.rb
@@ -4,7 +4,7 @@ class MessageCompletionsController < ApplicationController
       last_log = MessageLog.for_viewer(current_user).first
       assign_from_log(last_log) if last_log
     else
-      flow_item = FlowItem.find_by(id: session[:last_flow_item_id])
+      flow_item = FlowItem.find_by(key: session[:last_flow_item_key])
       assign_from_flow_item(flow_item) if flow_item
     end
   end
@@ -16,6 +16,7 @@ class MessageCompletionsController < ApplicationController
     @flow_item_icon              = log.flow_item_icon
     @flow_item_icon_color        = log.flow_item_icon_color
     @message_category_name       = log.message_category_name
+    @detail_flow_text            = log.detail_flow_text
   end
 
   def assign_from_flow_item(flow_item)
@@ -23,5 +24,23 @@ class MessageCompletionsController < ApplicationController
     @flow_item_icon              = flow_item.icon
     @flow_item_icon_color        = flow_item.icon_color
     @message_category_name       = flow_item.message_category.name
+    @detail_flow_text            = build_detail_flow_text_from_session(flow_item)
+  end
+
+  def build_detail_flow_text_from_session(flow_item)
+    df = session[:detail_flow]
+    return nil unless df&.dig("flow_item_key") == flow_item.key
+
+    answers    = df["answers"] || {}
+    definition = DetailFlowDefinitions.find(flow_item.detail_flow_key)
+    return nil unless definition
+
+    parts = definition[:steps].filter_map do |step|
+      ans = answers[step[:key]]
+      next unless ans
+      "#{step[:label]}：#{ans['label']}"
+    end
+
+    parts.join("、").presence
   end
 end
--- app/controllers/message_logs_controller.rb
+++ app/controllers/message_logs_controller.rb
@@ -6,15 +6,41 @@ class MessageLogsController < ApplicationController
   end
 
   def create
-  flow_item = FlowItem.find(params[:flow_item_id])
+    flow_item = FlowItem.find_by!(key: params[:flow_item_key])
 
     if user_signed_in?
-      MessageLog.create!(user: current_user, flow_item: flow_item)
-      flash[:notice] = "ログに自動保存されました"
+      detail_text, detail_data = build_detail_flow(flow_item)
+      MessageLog.create!(
+        user: current_user,
+        flow_item: flow_item,
+        detail_flow_text: detail_text,
+        detail_flow_data: detail_data
+      )
+      session.delete(:detail_flow)
+      flash[:notice] = t("message_logs.created")
     else
-      session[:last_flow_item_id] = flow_item.id
+      session[:last_flow_item_key] = flow_item.key
     end
 
     redirect_to message_completion_path
+  end
+
+  private
+
+  def build_detail_flow(flow_item)
+    df = session[:detail_flow]
+    return [ nil, nil ] unless df&.dig("flow_item_key") == flow_item.key
+
+    answers    = df["answers"] || {}
+    definition = DetailFlowDefinitions.find(flow_item.detail_flow_key)
+    return [ nil, nil ] unless definition
+
+    parts = definition[:steps].filter_map do |step|
+      ans = answers[step[:key]]
+      next unless ans
+      "#{step[:label]}：#{ans['label']}"
+    end
+
+    [ parts.join("、").presence, answers.presence ]
   end
 end
