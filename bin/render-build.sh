bac7dc39896ac18bb9509b9edcc016ef0c5f01ae
masagin <naganobol6212@gmail.com>
2026-05-10 20:48:02 +0900

    fix: migrationでNeonDBのdirect URLを使うようrender-build.shを修正する

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,4 +3,6 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+
+# migration 時のみ Direct URL を使う（NeonDB PgBouncer 回避）
+DATABASE_URL="$DATABASE_URL_DIRECT" bundle exec rails db:migrate
