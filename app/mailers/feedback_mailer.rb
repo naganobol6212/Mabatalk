0f78902e75b476ee465ea68697dccba9200617c7
masagin <naganobol6212@gmail.com>
2026-05-10 10:44:23 +0900

    fix: Mailerでadmin email未設定時に明示的にエラーを出す

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -2,6 +2,7 @@ class FeedbackMailer < ApplicationMailer
   def notify_admin(feedback)
     @feedback = feedback
     admin_email = Rails.application.credentials.dig(:admin, :email)
+    raise "Admin emails not configured (credentials.admin.email)" if admin_email.blank?
 
     mail(
       to: admin_email,
